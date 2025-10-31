f6c3221462518d21924a7d547690ac2e76a6bf2d
Datchani <d.imnoy@tlco.it>
2025-10-31 12:56:29 +0100

    add the slide

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
+import React from 'react';
+import { Dimensions, View } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 import ShopCartsComponent from '@/components/ShopCartsComponent';
 import { shops } from '@/dummydata';
-import {  FlatList, Text, View } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 export default function Homepage() {
   return (
-    <FlatList
-      data={shops}
-      renderItem={({item}) => <ShopCartsComponent shop={item} />}
-    />
+    <View className="flex-1 bg-black">
+      <Carousel
+        width={width}        // each card slightly narrower than the screen
+        data={shops}
+        loop
+        autoPlay
+        autoPlayInterval={3000}
+        mode="parallax"             
+        scrollAnimationDuration={1000}
+        style={{ alignSelf: 'center' }}
+        renderItem={({ item }) => (
+          <ShopCartsComponent shop={item} />
+        )}
+      />
+    </View>
   );
 }
