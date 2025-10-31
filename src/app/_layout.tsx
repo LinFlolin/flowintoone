e2a83216ed4113384228fea6f73029d766974e08
Datchani <d.imnoy@tlco.it>
2025-10-31 10:46:41 +0100

    theme config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,14 @@
-import { Slot } from 'expo-router'
-import React from 'react'
+// app/_layout.tsx
+import { Slot } from 'expo-router';
+import React from 'react';
+import { ThemeProvider, DarkTheme, DefaultTheme } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 export default function MainLayout() {
-  return <Slot/> 
+  const scheme = useColorScheme();
+  return (
+    <ThemeProvider value={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
