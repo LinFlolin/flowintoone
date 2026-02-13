a515fbc3e55a027e862a9c9c5a3a519fe2e17592
Datchani <d.imnoy@tlco.it>
2026-02-13 15:47:29 +0100

    shop page

--- app/components/shop/[slug]/page.tsx
+++ app/components/shop/[slug]/page.tsx
@@ -3,7 +3,7 @@ import ShopForm from '@/app/ui/utils/shopForm';
 export default function DashboardPage() {
   return (
     <main className="p-6">
-      {/* <ShopForm /> */}
+      <ShopForm />
     </main>
   );
 }
