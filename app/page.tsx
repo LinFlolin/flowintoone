5a5ac75e74eeacf5aafeb6eac03ac7577c798224
Lin <datchanilinza@gmail.com>
2026-06-22 14:28:44 +0200

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,18 @@ import QA from "./components/Flowcomponent/home/QA";
 import WhoWeAre from "./components/Flowcomponent/home/WhoWeAre";
 export default async function Home() {
   return (
-  //  <div className="">
-  //     <HeroHome />
-  //     <WhoWeAre />
-  //     <BecomePartner />
-  //     <QA />      
-  //  </div>
-  <div>
-      
-  </div>
+    //  <div className="">
+    //     <HeroHome />
+    //     <WhoWeAre />
+    //     <BecomePartner />
+    //     <QA />      
+    //  </div>
+    <div className="bg-linear-to-b from-DarkViola from-0% via-GrapeViola via-35% to-pinkViola to-100%  h-screen w-screen flex items-center justify-center p-6">
+      <div className="text-white text-3xl font-bold">
+        <h1>Lorem Ipsum</h1>
+        <p className="font-light text-sm">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+        </p>
+      </div>
+    </div>
   );
 }
